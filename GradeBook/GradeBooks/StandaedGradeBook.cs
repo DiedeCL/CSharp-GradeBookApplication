eb81390d246fb6250142c5d67f9d1688c62fbb47
DiedeClaessens <diede.claessens@student.pxl.be>
2019-05-30 15:19:45 +0200

    Completed the module

---
+++ GradeBook/GradeBooks/StandaedGradeBook.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    public class StandaedGradeBook : BaseGradeBook
+    {
+        public StandaedGradeBook(string name) : base(name)
+        {
+            Type = Enums.GradeBookType.Standard;
+        }
+    }
+}
